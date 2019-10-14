4a9d3777c97ee6de0bc255c4ce2da2e55d990682
qwertyuiop232 <vk07275@gmail.com>
2019-10-14 11:17:46 -0400

    added simple register route with error checking

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,6 @@ const homeRoute = require('./controllers/homepage')
 const registerRoute = require('./controllers/register')
 
 function init(server) {
-
 	server.get('*', function(req, res, next) {
 		console.log('Request was made to: ' + req.originalUrl);
 		return next();
