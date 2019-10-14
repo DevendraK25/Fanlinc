e4aa2b3f4e38d1066ae4bfc3023a429a51908e43
qwertyuiop232 <vk07275@gmail.com>
2019-10-14 11:18:09 -0400

    fixed unique indexes with user schema

--- src/db/dbDriver.js
+++ src/db/dbDriver.js
@@ -1,16 +1,72 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose')
+const User = require('../model/user')
 
-
-// Get DB to connect to 
+// Get DB to connect to
 // move to .env file later
- mongoose.connect('mongodb+srv://user:Password@cluster0-pu5as.mongodb.net/test?retryWrites=true&w=majority', {
-	useNewUrlParser : true,
-	useUnifiedTopology : true
-});
-
+mongoose
+		.connect(
+				'mongodb+srv://user:Password@cluster0-pu5as.mongodb.net/test?retryWrites=true&w=majority',
+				{
+					useNewUrlParser : true,
+					useUnifiedTopology : true,
+					useCreateIndex : true
+				});
 function verifyUser(user, password) {
+	var found = mongoose.Collection.findOne({
+		username : user,
+		password : password
+	});
+	if (found) {
+		return found;
+	} else {
+		return false;
+	}
+}
+
+function addUser(jsonUser, method) {
+	console.log(jsonUser)
+
+	var newUser = new User({
+		username : jsonUser.username,
+		password : jsonUser.password,
+		email : jsonUser.email
+	})
 	
 	
+//	newUser.save(function(err) {
+//		if (err) {
+//			console.log(err.message)
+//			method(400);
+//		} else {
+//			method(200);
+//		}
+//	})
+	
+	newUser.save().then(function() {
+		console.log("saved")
+	}).then(function() {
+		method(200)
+	}, function(err) {
+		if(err) {
+			console.log(err.message)
+			method(400)
+		}
+	})
+
+
+	
+
+//	try {
+//	newUser.save().then( () => {
+//		return 200
+//	})
+//	} catch(err){
+//		return 400
+//	}
+	
+}
+
+module.exports = {
+	verifyUser : verifyUser,
+	addUser : addUser
 };
- 
-module.exports = mongoose;
--- src/model/user.js
+++ src/model/user.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
+
+
 const userSchema = new mongoose.Schema({
 	username : {
 		type : String,
-		required : true
+		required : true,
+		unique: true
 	},
 	password : {
 		type : String,
@@ -13,9 +16,13 @@ const userSchema = new mongoose.Schema({
 	},
 	email : {
 		type : String,
-		required : true
+		required : true,
+		unique: true
 	}
+});
 
+mongoose.model('User', userSchema).ensureIndexes(function (err) {
+  if (err) console.log(err.message);
 });
 
 module.exports = mongoose.model('User', userSchema);
