fc51a27d85ede9554e99456a782883ea0c0f6b03
qwertyuiop232 <vk07275@gmail.com>
2019-11-17 23:57:35 -0500

    added update and delete for posts

--- backend/handlers/postH.js
+++ backend/handlers/postH.js
@@ -9,7 +9,7 @@ router.get('/:id', function(req, res){ pdb.addPost(req, res) });
 router.delete('/deleteAll', function(req, res){ pdb.deleteAll(req, res) });
 
 // TODO
-router.post('/update/:id', function(req, res){res.sendStatus(400) });
-router.delete('/delete/:id', function(req, res){res.sendStatus(400) });
+router.post('/update/:id', function(req, res){pdb.updatepost(req, res) });
+router.delete('/delete/:id', function(req, res){pdb.deletepost(req, res) });
 
 module.exports = router;
